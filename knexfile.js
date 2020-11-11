7b5d1dea9e6c1eb9cb103818e9dfac77de283fdb
LucasBrotto <lewbrotto@gmail.com>
2020-11-11 20:29:59 -0300

    fix knex

--- knexfile.js
+++ knexfile.js
@@ -17,7 +17,12 @@ module.exports = {
   },  
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    connection: {
+      host: 'ec2-34-237-247-76.compute-1.amazonaws.com',
+      database: 'd1gcv8b66d852j',
+      user:     'pvkdfguerkekcr',
+      password: '1e9ed30095ff11dfa29cf5f94ed7996c4049f55dded10b2021ab01e06f8cad76'
+    },
     migrations: {
       directory: __dirname + '/src/migrations',
     },
