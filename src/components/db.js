504db4a30438b26c17cb875aa9dc35bb685ce472
LucasBrotto <lewbrotto@gmail.com>
2020-11-11 20:51:32 -0300

    init deploy

--- src/components/db.js
+++ src/components/db.js
@@ -1,14 +1,4 @@
-const config = require('../../knexfile')
-const knex = require('knex')
-const env = {
-    host: 'ec2-34-237-247-76.compute-1.amazonaws.com',
-    database: 'd1gcv8b66d852j',
-    user:     'pvkdfguerkekcr',
-    password: '1e9ed30095ff11dfa29cf5f94ed7996c4049f55dded10b2021ab01e06f8cad76'
-}
+const config = require('../knexfile.js')
+const knex = require('knex')(config)
 
-const db = knex(config[env])
-
-console.log(db)
-
-module.exports = db
+module.exports = knex
