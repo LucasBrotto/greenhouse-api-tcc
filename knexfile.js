d9b48b7f2b14b107d71c644f9bc626acf9709826
LucasBrotto <lewbrotto@gmail.com>
2020-11-11 21:00:59 -0300

    init deploy

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,5 @@
 module.exports = {
-  client: 'postgresql',
+  client: 'pg',
   connection: {
     host: 'ec2-34-237-247-76.compute-1.amazonaws.com',
     database: 'd1gcv8b66d852j',
@@ -8,7 +8,6 @@ module.exports = {
   },
   migrations: {
     directory: __dirname + '/src/migrations',
-    tableName: 'knex_migrations'
   },
   pool: {
     min: 2,
